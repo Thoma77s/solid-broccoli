d07524e44b2aa864bad642be0404415e9cf412c1
Thoma77s <thomassiffc3@gmail.com>
2022-05-03 20:29:11 -0400

    added schema folder, and added index, and resolver code

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,55 @@
+const { User, Book } = require('../models');
+const { AuthenicationError } = require('apollo=server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+  me: async (parent, args, context) => {
+    if (context.user) {
+      const userData = await User.findOne({ _id: context.user._id })
+      .select('-__v -password')
+      return userData;
+    }
+    throw new AuthenticationError('You are not logged in!');
+  }
+},
+Mutation: {
+  addUser: async (parent, args) => {
+    const user = await User.create(args);
+    const token = signToken(user);
+
+    return { token, user };
+  },
+  login: async (parent, { email, password }) => {
+    const user = await User.findOne( { email });
+    if (!user) {
+      throw new AuthenticationError('Invalid login information');
+    }
+    const token = signToken(user);
+    return { token, user };
+  },
+  saveBook: async (parent, { book }, context) => {
+    if (context.user) {
+      const updatedUser = await User.findOneAndUpdate(
+        {_id: context.user._id},
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      )
+      return updatedUser;
+    }
+  },
+  removeBook: async (parent, args, context) => {
+    if(context.user) {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: args.bookId } } },
+        { new: true }
+      );
+      return updatedUser
+    }
+    throw new AuthenticationError('You must be logged in!');
+  }
+}
+};
+
+module.exports = resolvers
